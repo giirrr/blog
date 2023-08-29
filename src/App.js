04b939082f9d8ea1b315739ac67c1b6e969feb47
giirrr <ikl1121@naver.com>
2023-08-30 05:25:41 +0900

    first homework done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집";
+  //자료 잠깐 저장할 땐 변수
+  let [a, b] = useState(["남자 코드 추천", "강남 우동맛집", "파이썬 독학"]);
+  let [a2, b2] = useState(["17", "18", "19"]);
+  let [logo, setLogo] = useState("ReactBlog");
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ color: "#FCEB73", fontSize: "16px" }}>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      <h4>{post}</h4>
+
+      <div className="list">
+        <h4>{a[0]}</h4>
+        <p>2월 {a2[0]}일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{a[1]}</h4>
+        <p>2월 {a2[1]}일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{a[2]}</h4>
+        <p>2월 {a2[2]}일 발행</p>
+      </div>
     </div>
   );
 }
